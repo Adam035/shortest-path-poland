22f65f12e0fb2a52b12c6930a953abf21c147765
Adam Lipian <adamlipian2723@gmail.com>
2024-07-10 00:32:57 +0200

    feat: spring app

---
+++ src/main/java/com/lipian/shortestpathpoland/ShortestPathApp.java
@@ -0,0 +1,11 @@
+package com.lipian.shortestpathpoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortestPathApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortestPathApp.class, args);
+    }
+}
